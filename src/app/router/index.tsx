44e893fda265ba3fe6b3d1e6f849c56bbbb1cdbf
queenJiJi <lovebellaberry52@gmail.com>
2026-01-23 17:11:38 +0900

    feat: routing the login page

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignupPage from "@/features/auth/pages/SignupPage";
 import MainPage from "@/features/main/pages/MainPage";
+import LoginPage from "@/features/auth/pages/LoginPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <MainPage /> },
   { path: "/auth/signup", element: <SignupPage /> },
+  { path: "/auth/login", element: <LoginPage /> },
 ]);
---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <header>여기가 로그인페이지에요.</header>;
+}
