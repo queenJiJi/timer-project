bb87975d9d7713f350a2ab9215a3c489534bf8dc
queenJiJi <lovebellaberry52@gmail.com>
2026-01-24 13:36:28 +0900

    feat: implement the logout function

--- src/features/main/pages/MainPage.tsx
+++ src/features/main/pages/MainPage.tsx
@@ -1,7 +1,23 @@
+import UseLogoutMutation from "@/features/auth/model/useLogoutMutation";
+import { useNavigate } from "react-router-dom";
+
 export default function MainPage() {
+  const navigate = useNavigate();
+  const logoutMutation = UseLogoutMutation();
+
+  const onLogout = async () => {
+    try {
+      await logoutMutation.mutateAsync();
+    } finally {
+      navigate("/auth/login", { replace: true });
+    }
+  };
+
   return (
     <div>
       <h1>여기가 메인(타이머)페이지에요</h1>
+      {/* 임시로 logout 버튼 넣어둔 것 */}
+      <button onClick={onLogout}>로그아웃</button>
     </div>
   );
 }
